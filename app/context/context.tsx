24820bd570679f4d0f5302206272eb47ebb34a84
Kshitij Chauhan <kshitij.cx@gmail.com>
2024-08-13 22:40:00 +0530

    states declared and passed through context

--- app/context/context.tsx
+++ app/context/context.tsx
@@ -1,27 +1,38 @@
-"use client"
-import { createContext } from "react";
+"use client";
+import { createContext, useState } from "react";
 import run from "../config/gemini";
 
 export const Context = createContext({});
 
-const ContextProvider = (props:any) => {
-     
-    const [input,setInput] =
+const ContextProvider = (props: any) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompts, setPrevPrompts] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
 
-    const onSent = async(prompt:string) => {
-        await run(prompt)
-    }
+  const onSent = async (prompt: string) => {
+    await run(prompt);
+  };
 
-    onSent("What is react?")
+  onSent("What is react?");
 
-    const contextValue = {
+  const contextValue = {
+    prevPrompts,
+    setPrevPrompts,
+    onSent,
+    recentPrompt,
+    setRecentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput, 
+  };
 
-    }   
-
-    return (
-        <Context.Provider value={contextValue}>
-            {props.children}
-        </Context.Provider>
-    )
-}
-export default ContextProvider
+  return (
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  );
+};
+export default ContextProvider;
